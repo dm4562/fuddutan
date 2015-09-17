9666164dd5194338562d2446d20e4e3932ae46ac
Dhruv Mehra <dmehra6@gatech.edu>
2015-09-17 15:55:43 -0400

    Working on Log in and Log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
-  def new
-  end
+    def new
+    end
+
+    def create
+        user = User.find_by email: params[:sessions][:email].downcase
+
+        if user && user.authenticate(params[:sessions][:password])
+            #Log the user in and redirect to the user page
+        else
+            flash[:danger] = 'Invalid email/password combination'
+            render 'new'
+        end
+    end
+
+    def destroy
+    end
 end
