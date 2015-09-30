60a96873f52aebf20744ed20e12d5603c63ab668
Dhruv Mehra <dmehra6@gatech.edu>
2015-09-29 23:51:05 -0400

    Still working on the other table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ActiveRecord::Base
+  validates :type, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :has_id, presence: true
+  validates :where_found, presence: true
+  validates :where_held, presence: true
+  validates :connected_lost_items, presence: true
+  validates :description, presence: true, length: { maximum: 65536 } 
 end
