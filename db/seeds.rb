4042f7d1659fc52ac2d1fda22d8aa10662ff514d
Dhruv Mehra <dmehra6@gatech.edu>
2015-10-06 19:30:25 -0400

    Added item types from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib','seeds','item_types.csv'))
+csv_data = CSV.parse csv_text, headers: true
+csv_data.each do |row|
+  # puts row.to_hash
+  item_type = ItemType.new
+  item_type.name = row["name"]
+  item_type.save
+  puts "#{item_type.name} saved!"
+end
